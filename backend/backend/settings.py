6f5232ae7b85a29089fa275baa935222391a587f
HuaVinh <quocvinh12360@gmail.com>
2022-01-28 22:42:39 +0700

    update back

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&93g9(#f64akr+v&3e5u$8v%6u*@y=bq!xn4x5e3pjw(i59=o@'
+SECRET_KEY = 'django-insecure-*9ys7nau=2q$)k1)xp3s(3kdgps!u(m80l5lqw%v8i#)5!0o#+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todo'
+    'todo',
 ]
 
 MIDDLEWARE = [
--- backend/todo/admin.py
+++ backend/todo/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-
 from .models import Todo
 
 class TodoAdmin(admin.ModelAdmin):
